33107966f1adc7837c2d2c135836c3f92668eeec
KevinBolanosDev <im.kevinbolanos.dev@gmail.com>
2023-12-18 18:23:44 -0500

    defining details of the web model

--- scripts/script.js
+++ scripts/script.js
@@ -99,3 +99,16 @@ toggleAnimationClass();
 
 setInterval(toggleAnimationId, 30000);
 setInterval(toggleAnimationClass, 10000);
+
+const navToggle = document.querySelector(".nav__toggle");
+const navMenu = document.querySelector(".nav__menu");
+
+navToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("nav__menu-visible");
+
+  if (navMenu.classList.contains("nav__menu-visible")) {
+    navToggle.setAttribute("aria-label", "Cerrar menú");
+  } else {
+    navToggle.setAttribute("aria-label", "Abrir menú");
+  }
+});
